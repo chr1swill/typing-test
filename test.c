3158b70014a2153ba9b9c101e20a858b53609ae5
chr1swill <alexanderwilliams20003@gmail.com>
2025-06-17 15:53:17 -0400

    feat: newline is added at end of worklist fix: memset with space char instead of '\0' chars

--- test.c
+++ test.c
@@ -78,7 +78,7 @@ typedef struct
 } Span;
 
 static inline
-void Span_make(Span *span, size_t len)
+void Span_make(Span *span, size_t len, char c)
 {
   span->bytes = malloc(sizeof(char) * len);
   if (span->bytes == NULL)
@@ -86,7 +86,7 @@ void Span_make(Span *span, size_t len)
     perror("Span_new: malloc");
     exit(EXIT_FAILURE);
   }
-  memset(span->bytes, 0, len);
+  memset(span->bytes, c, len);
 
   span->len = len;
 }
@@ -103,7 +103,7 @@ static inline
 void make_wordlist(Span *span, StrSpan *ss,
                    int *randnums, size_t randnums_len)
 {
-  size_t i; 
+  size_t i, str_len; 
   char *str;
   char *cursor;
 
@@ -114,23 +114,44 @@ void make_wordlist(Span *span, StrSpan *ss,
   }
 
   // for the spaces in between words
-  span->len += (randnums_len - 1); 
+  //span->len += (randnums_len - 1); 
+  span->len += randnums_len; 
 
-  Span_make(span, span->len);
-  printf("span->len=%ld\n", span->len);
+  Span_make(span, span->len, ' ');
+  //printf("span->len=%ld\n", span->len);
 
   i = 0;
   cursor = &span->bytes[0]; 
   for (;i < randnums_len; ++i)
   {
     str = ss->strarr[randnums[i]];
+    str_len = strlen(str);
 
-    memmove(cursor, str, strlen(str)); 
-    cursor += strlen(str) + 1;
+    memmove(cursor, str, str_len); 
+    cursor += str_len + 1;
+    // if (i != randnums_len)
+    // {
+    //   cursor += str_len + 1;
+    // }
+    // else
+    // {
+    //   cursor[str_len + 1] = '\n';
+    // }
   }
 
-  assert(span->len == (cursor - span->bytes) - 1);
+  span->bytes[span->len - 1] = '\n';
+
+  assert(span->len == (cursor - span->bytes));
 } 
+
+#define usage() \
+do { \
+  printf("usage: %s [number of words in test]\n"        \
+          "example: %s 4 // creates a 4 word test\n"    \
+          "exmaple: %s 50 // creates a 50 word test\n", \
+          argv[0], argv[0], argv[0]);                   \
+  exit(EXIT_FAILURE);                                   \
+} while(0); \
 
 int main(int argc, char **argv)
 {
@@ -141,19 +162,23 @@ int main(int argc, char **argv)
 
   if (argc < 2)
   {
-    printf("brother fix that\n");
-    return 1;
+    usage();
   }
 
   i = atol(argv[1]);
-  printf("words_in_test as number=%ld\n", i);
+  if (i < 1)
+  {
+    usage();
+  }
+
+  //printf("words_in_test as number=%ld\n", i);
   
   int randoz[i];
   const size_t randoz_len = i;
 
   StrSpan_from_linefeed_delim_bytes(&ss,
     american_english, american_english_len);
-  printf("ss.len=%ld\n", ss.len);
+  //printf("ss.len=%ld\n", ss.len);
 
   srand(time(NULL) - randoz_len);
 
