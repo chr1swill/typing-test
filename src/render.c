0aacaf6ec09226b8d6ae66fe883d8f1e192cb833
chr1swill <alexanderwilliams20003@gmail.com>
2025-06-18 19:39:49 -0400

    feat: make background highlight on error instead of char

--- src/render.c
+++ src/render.c
@@ -26,14 +26,14 @@ void putchar_at_offset_white(struct termios *oldt, size_t offset, char c)
   switch (offset)
   {
     case 0:
-      if ((printf("\033[H\033[B\033[97m%c", c)) == -1)
+      if ((printf("\033[H\033[B\033[40m%c", c)) == -1)
       {
         perror("putchar_at_offset: offset 0");
         clean_on_exit(oldt, EXIT_FAILURE);
       }
     break;
     default:
-      if ((printf("\033[H\033[B\033[%zuC\033[97m%c",
+      if ((printf("\033[H\033[B\033[%zuC\033[40m%c",
            offset, c)) == -1)
       {
         perror("putchar_at_offset: offset greater than 0");
@@ -49,14 +49,14 @@ void putchar_at_offset_red(struct termios *oldt, size_t offset, char c)
   switch (offset)
   {
     case 0:
-      if ((printf("\033[H\033[B\033[31m%c", c)) == -1)
+      if ((printf("\033[H\033[B\033[41m%c", c)) == -1)
       {
         perror("putchar_at_offset: offset 0");
         clean_on_exit(oldt, EXIT_FAILURE);
       }
     break;
     default:
-      if ((printf("\033[H\033[B\033[%zuC\033[31m%c",
+      if ((printf("\033[H\033[B\033[%zuC\033[41m%c",
            offset, c)) == -1)
       {
         perror("putchar_at_offset: offset greater than 0");
